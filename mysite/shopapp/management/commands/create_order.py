a1358cb0632be4e654f970b998c28c767c635be7
Dimon <dimon3182@k66.ru>
2023-01-30 16:02:55 +0500

    Join create_order.py and update_order.py, first attempt

--- mysite/shopapp/management/commands/create_order.py
+++ mysite/shopapp/management/commands/create_order.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand
-from shopapp.models import Order
+from shopapp.models import Order, Product
 
 
 class Command(BaseCommand):
@@ -16,3 +16,16 @@ class Command(BaseCommand):
             user=user,
         )
         self.stdout.write(f"Created order {order}")
+
+        order = Order.objects.first()
+        products = Product.objects.all()
+        for product in products:
+            order.products.add(product)
+
+        order.save()
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully added products{order.products.all()} to order {order}"
+            )
+        )
