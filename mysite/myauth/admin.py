5058c3664fdafa0a568a7a10bc8b62e27f01a3cf
Dimon <dimon3182@k66.ru>
2023-04-14 16:31:29 +0500

    did homework #11, add admin model Profile

--- mysite/myauth/admin.py
+++ mysite/myauth/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = "pk", "profile_name", "bio", "avatar",
+
+    def profile_name(self, obj: Profile) -> str:
+        return obj.user.username
